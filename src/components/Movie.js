1a2360e7d9492a3111bf7f4564f033f6918828c8
chiree sherpa <tsherisherpa@gmail.com>
2024-03-05 17:23:42 -0500

    decoupled movie component from movie list and added is favourite logic

---
+++ src/components/Movie.js
@@ -0,0 +1,46 @@
+import { View, Text, TouchableWithoutFeedback, Image,Dimensions } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { fallbackMoviePoster, image185 } from '../../api/moviesdb'
+import { HeartIcon } from 'react-native-heroicons/solid'
+import { useDispatch, useSelector } from 'react-redux'
+import {toggleFavorite, selectIsFavourite } from '../redux/reducers/favoriteSlice'
+
+let { width, height } = Dimensions.get('window')
+
+export const Movie = ({item, index}) => {
+
+    const navigation = useNavigation()
+
+    const dispatch = useDispatch()
+    let favorite = useSelector((state) => selectIsFavourite(state, item.id))
+
+    const handleFavoriteToggle = (id) => {
+        dispatch(toggleFavorite(id))
+    }
+
+    return (
+        <TouchableWithoutFeedback key={index} onPress={() => navigation.push("Movie", item)}>
+            <View className="space-y-4 mr-4">
+                <Image
+                    source={{ uri: image185(item.poster_path) || fallbackMoviePoster }}
+                    className="rounded-3xl"
+                    style={{
+                        width: width * 0.33,
+                        height: height * 0.22,
+                    }}
+                />
+                <Text className="text-neutral-300 ml-1">{
+                    item.title.length > 14 ? item.title.slice(0, 14) + '...' : item.title
+                }
+                </Text>
+
+                <View style={{ position: 'absolute', top: -5, right: 5 }}>
+                    <TouchableWithoutFeedback onPress={() => handleFavoriteToggle(item.id)}>
+                        <HeartIcon size={28} strokeWidth={2.5} color={favorite ? "red" : "white"} />
+                    </TouchableWithoutFeedback>
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    )
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,12 @@
-import { View, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image,Dimensions } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { styles } from '../theme'
 import { useNavigation } from '@react-navigation/native'
-import { fallbackMoviePoster, image185 } from '../../api/moviesdb'
-
-let { width, height } = Dimensions.get('window')
+import { Movie } from './Movie'
 
 export default function MovieList({ title,hideSeeAll, data }) {
     const navigation = useNavigation()
-
+    
     return (
         <View className="mb-8 space-y-4">
             <View className="mx-4 flex-row justify-between items-center">
@@ -26,29 +24,10 @@ export default function MovieList({ title,hideSeeAll, data }) {
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ paddingHorizontal: 15 }}
-
             >
                 {
                     data.map((item, index) => (
-                        <TouchableWithoutFeedback key={index} onPress={() => navigation.push("Movie", item)}>
-                            <View className="space-y-4 mr-4">
-                                <Image
-                                    // source={require('../assets/images/moviePoster2.png')}
-                                    source={{uri:image185(item.poster_path)|| fallbackMoviePoster}}
-                                    className="rounded-3xl"
-                                    style={{
-                                        width: width*0.33,
-                                        height: height*0.22,
-                                    }}
-                                />
-                                <Text className="text-neutral-300 ml-1">{
-                                    item.title.length>14 ? item.title.slice(0,14)+'...' : item.title
-                                    
-                                }
-                                </Text>
-                            </View>
-
-                        </TouchableWithoutFeedback>
+                        <Movie item={item} key={index}/>
                     ))
                 }
             </ScrollView>
