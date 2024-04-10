a84810186330829d3b73c9c93362da9113bb7400
chiree sherpa <tsherisherpa@gmail.com>
2024-04-09 20:28:03 -0400

    [#12] handled backpress from homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Platform, ScrollView } from "react-native";
+import { View, Platform, ScrollView, BackHandler, Alert } from "react-native";
 import TrendingMovies from "../components/TrendingMovies.js";
 import { useEffect, useState } from "react";
 import MovieList from "../components/MovieList";
@@ -21,8 +21,27 @@ const HomeScreen = () => {
         getTrendingMovies()
         getUpcomingMovies()
         getTopRatedMovies()
+
+        //handling backpress from home screen
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction
+        );
+
+        return () => backHandler.remove();
     }, [])
 
+    const backAction = () => {
+        Alert.alert('Exit', 'Are you sure you want to exit?', [
+            {
+                text: 'Cancel',
+                onPress: () => null,
+                style: 'cancel',
+            },
+            { text: 'YES', onPress: () => BackHandler.exitApp() },
+        ]);
+        return true;
+    };
 
     //  getting trending movies
     const getTrendingMovies = async () => {
